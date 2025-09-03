7f2e918006dfed79bd1272435bf565cb65afeb0e
Dominique Unruh <git.qischair.51byei@rwth.unruh.de>
2025-09-03 11:51:10 +0200

    Using WORKDIR env

--- api/public/parseexpression.php
+++ api/public/parseexpression.php
@@ -10,8 +10,8 @@ require_once(__DIR__ . '/../../stack/potentialresponsetreestate.class.php');
 
 //require(__DIR__ . '/../vendor/autoload.php');
 
-//$xml = file_get_contents('question.xml');
-//$question = StackQuestionLoader::loadxml($xml)['question'];
+//$xml = file_get_contents('/workdir/question.xml');
+//$question = \api\util\StackQuestionLoader::loadxml($xml)['question'];
 //StackSeedHelper::initialize_seed($question, "aufhasdiufyretgfdg");
 //$question->id = "some_id";
 //$question->castextprocessor = new \castext2_qa_processor(new \stack_outofcontext_process());
@@ -32,10 +32,11 @@ require_once(__DIR__ . '/../../stack/potentialresponsetreestate.class.php');
 //stack_cas_configuration::create_maximalocal();
 //stack_cas_configuration::create_auto_maxima_image();
 
+$workdir = getenv('WORKDIR') ?: '.';
 $options = new stack_options();  // Maybe useful for tweaking parser
 $el = stack_input_factory::make('algebraic', 'sans1', "");  // There are some extra arguments here, maybe useful for tweaking parser
 
-$expression = file_get_contents("/workdir/expression.txt");
+$expression = file_get_contents("$workdir/expression.txt");
 $state = $el->validate_student_response(['sans1' => $expression], $options, '', new stack_cas_security());
 print_r($state);
-file_put_contents("/workdir/result.txt", $state->contentsdisplayed);
+file_put_contents("$workdir/result.txt", $state->contentsdisplayed);
